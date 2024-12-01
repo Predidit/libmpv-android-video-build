2920d56ab191cadbea977cf84e647c8fb79546c6
Predidit <34627277+Predidit@users.noreply.github.com>
2024-12-01 10:07:49 +0800

    revert to 186205a

--- buildscripts/bundle_default.sh
+++ buildscripts/bundle_default.sh
@@ -30,30 +30,55 @@ sudo chmod +x gradlew
 
 unzip -o app/build/outputs/apk/release/app-release.apk -d app/build/outputs/apk/release
 
-cp app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so      ../../prefix/arm64-v8a/usr/local/lib
-cp app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so    ../../prefix/armeabi-v7a/usr/local/lib
-cp app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so            ../../prefix/x86/usr/local/lib
-cp app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so         ../../prefix/x86_64/usr/local/lib
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/arm64-v8a"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/armeabi-v7a"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86_64"
 
 cd ../..
 
-mkdir -p temp/lib/arm64-v8a
-mkdir -p temp/lib/armeabi-v7a
-mkdir -p temp/lib/x86
-mkdir -p temp/lib/x86_64
+# --------------------------------------------------
 
-cp prefix/arm64-v8a/usr/local/lib/*.so temp/lib/arm64-v8a/
-cp prefix/armeabi-v7a/usr/local/lib/*.so temp/lib/armeabi-v7a/
-cp prefix/x86/usr/local/lib/*.so temp/lib/x86/
-cp prefix/x86_64/usr/local/lib/*.so temp/lib/x86_64/
+cd deps/media_kit/media_kit_native_event_loop
 
-cd temp
+flutter create --org com.alexmercerind --template plugin_ffi --platforms=android .
 
-zip -r ../default-arm64-v8a.jar lib/arm64-v8a
-zip -r ../default-armeabi-v7a.jar lib/armeabi-v7a
-zip -r ../default-x86.jar lib/x86
-zip -r ../default-x86_64.jar lib/x86_64
+if ! grep -q android "pubspec.yaml"; then
+  printf "      android:\n        ffiPlugin: true\n" >> pubspec.yaml
+fi
 
-cd ../
+flutter pub get
+
+cp -a ../../mpv/libmpv/. src/include/
+
+cd example
+
+flutter clean
+flutter build apk --release
+
+unzip -o build/app/outputs/apk/release/app-release.apk -d build/app/outputs/apk/release
+
+cd build/app/outputs/apk/release/
+
+# --------------------------------------------------
+
+rm -r lib/*/libapp.so
+rm -r lib/*/libflutter.so
+
+zip -r "default-arm64-v8a.jar"                lib/arm64-v8a
+zip -r "default-armeabi-v7a.jar"              lib/armeabi-v7a
+zip -r "default-x86.jar"                      lib/x86
+zip -r "default-x86_64.jar"                   lib/x86_64
+
+mkdir -p ../../../../../../../../../../output
+
+cp *.jar ../../../../../../../../../../output
 
 md5sum *.jar
+
+cd ../../../../../../../../..
+
+# --------------------------------------------------
+
+zip -r debug-symbols-default.zip prefix/*/lib
+cp debug-symbols-default.zip ../output
