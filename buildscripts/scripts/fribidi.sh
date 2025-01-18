846abf9ef3d46f6dd39daf8e4a5cb57fa94d69b5
Predidit <34627277+Predidit@users.noreply.github.com>
2025-01-18 14:41:48 +0800

    remove fribidi build flag

--- buildscripts/scripts/fribidi.sh
+++ buildscripts/scripts/fribidi.sh
@@ -17,9 +17,6 @@ fi
 unset CC CXX # meson wants these unset
 
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-	-D{tests,docs}=false \
-	-Dc_extra_args="-D__DATE__=\"\" -D__TIME__=\"\" -frandom-seed=xyz" \
-  	-Dcpp_extra_args="-D__DATE__=\"\" -D__TIME__=\"\" -frandom-seed=xyz" 
-
+	-D{tests,docs}=false
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
