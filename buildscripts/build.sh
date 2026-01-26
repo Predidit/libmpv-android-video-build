d9776c821cc6d12c91400a1f492a250eea75e831
Predidit <34627277+Predidit@users.noreply.github.com>
2026-01-26 22:05:36 +0800

    try fix vk

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -91,6 +91,25 @@ cpu_family = '$cpu_family'
 cpu = '${CC%%-*}'
 endian = 'little'
 CROSSFILE
+
+	# Create Vulkan pkg-config file for Android NDK
+	local ndk_root="$ANDROID_HOME/ndk/$v_ndk"
+	local vulkan_api=21
+	local prebuilt_arch="linux-x86_64"
+	[[ "$OSTYPE" == "darwin"* ]] && prebuilt_arch="darwin-x86_64"
+	mkdir -p "$prefix_dir/lib/pkgconfig"
+	cat >"$prefix_dir/lib/pkgconfig/vulkan.pc" <<VULKAN_PC
+prefix=$ndk_root
+exec_prefix=\${prefix}
+includedir=\${prefix}/toolchains/llvm/prebuilt/${prebuilt_arch}/sysroot/usr/include
+libdir=\${prefix}/toolchains/llvm/prebuilt/${prebuilt_arch}/sysroot/usr/lib/${ndk_triple}/${vulkan_api}
+
+Name: Vulkan-Loader
+Description: Vulkan Loader
+Version: 1.3.${vulkan_api}
+Libs: -L\${libdir} -lvulkan
+Cflags: -I\${includedir}
+VULKAN_PC
 }
 
 build () {
