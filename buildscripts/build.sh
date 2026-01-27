36ecce11ca478011d801eba60fd1421f07f70909
Predidit <34627277+Predidit@users.noreply.github.com>
2026-01-27 12:10:30 +0800

    android sdk 29

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -16,7 +16,7 @@ getdeps () {
 loadarch () {
 	unset CC CXX CPATH LIBRARY_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH
 
-	local apilvl=24
+	local apilvl=29
 	# ndk_triple: what the toolchain actually is
 	# cc_triple: what Google pretends the toolchain is
 	if [ "$1" == "armv7l" ]; then
